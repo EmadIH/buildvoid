935984a38a17bc3e0eda81b1590d2b3c55c9a6c1
EmadIH <EmadIH1397@gmail.com>
2025-02-21 23:56:38 +0300

    Update suckless

---
+++ suckless/suckless/dwm/themes/blue.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#000000"; /* خلفية الثيم العامة  */
+static const char col_gray2[]       = "#444444"; /* بوردر النوفذة غير النشطة */
+static const char col_gray3[]       = "#6ebadb"; /* أرقام التاغات غير النشطة */
+static const char col_gray4[]       = "#ffffff"; /* أرقام التاغات النشطة */
+static const char col_cyan[]        = "#448ead"; /* الثيم العام وبوردر النوافذ النشطة */
